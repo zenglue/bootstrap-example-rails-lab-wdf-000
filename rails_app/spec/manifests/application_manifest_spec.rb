4a1dd3538459479d2fc59cd906198c8095bc6495
Carleton DiLeo <cbdileo@gmail.com>
2016-02-10 15:21:39 -0800

    Changed CSS manifest to allow for bootstrap gem

--- rails_app/spec/manifests/application_manifest_spec.rb
+++ rails_app/spec/manifests/application_manifest_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe do
   it "should require the bootstrap imports in the application CSS manifest" do
-    expect(File.read("app/assets/stylesheets/application.css")).to include('@import "bootstrap-sprockets";')
-    expect(File.read("app/assets/stylesheets/application.css")).to include('@import "bootstrap";')
+    expect(File.read("app/assets/stylesheets/application.scss")).to include('@import "bootstrap-sprockets";')
+    expect(File.read("app/assets/stylesheets/application.scss")).to include('@import "bootstrap";')
   end
 end
