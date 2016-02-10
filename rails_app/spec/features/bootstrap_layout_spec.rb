6daa1771a9e5604b0dcb4bf08be96a0bd758d5d6
Carleton DiLeo <cbdileo@gmail.com>
2016-02-10 15:28:54 -0800

    Change tests to use has_selector test

--- rails_app/spec/features/bootstrap_layout_spec.rb
+++ rails_app/spec/features/bootstrap_layout_spec.rb
@@ -11,18 +11,18 @@ feature "Bootstrap Layout" do
     end
 
     scenario "Page has 3 rows" do
-      expect(page).to assert_selector('div.row', :count => 3)
+      expect(page).to have_selector('div.row', count: 3)
     end
 
     scenario "Each row has 3 col-lg-4" do
-      expect(page).to assert_selector('div.col-lg-4', :count => 9)
+      expect(page).to have_selector('div.col-lg-4', count: 9)
     end
   end
 
   context 'Content' do
     scenario "Each col-lg-4 has <p> and bootstrap button" do
-      expect(page).to assert_selector('div.row div.col-lg-4 p', :count => 9)
-      expect(page).to assert_selector('div.row div.col-lg-4 .btn', :count => 9)
+      expect(page).to have_selector('div.row div.col-lg-4 p', count: 9)
+      expect(page).to have_selector('div.row div.col-lg-4 .btn', count: 9)
     end
   end
 
